87eeb421da3dee2e4dbbdc2f43778549c70272cb
Shivam <shivam2432000@gmail.com>
2022-11-13 09:04:59 +0530

    This is day-5 work

--- pharmacy_app/src/App.js
+++ pharmacy_app/src/App.js
@@ -1,12 +1,12 @@
 import Navbar from "./Components/Navbar";
 import "./App.css";
 import Home from "./Components/Home";
+import AllRoutes from "./Routes/AllRoutes";
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
-      <Home />
+      <AllRoutes />
     </div>
   );
 }
--- pharmacy_app/src/Routes/AllRoutes.jsx
+++ pharmacy_app/src/Routes/AllRoutes.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import LabTest from "../Components/LabTest";
-import { Wellness } from "../Components/Wellness";
+import Wellness from "../Components/Wellness";
+import Home from "../Components/Home";
 
 const AllRoutes = () => {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/Wellness" element={<Wellness />} />
       <Route path="/LabTest" element={<LabTest />} />
     </Routes>
